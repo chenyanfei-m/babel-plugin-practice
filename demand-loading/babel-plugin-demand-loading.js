8eb2366f92508ca9ccd8a51c832a71d513bb00ef
chenyanfei-m <1044927473@qq.com>
2019-01-22 16:03:44 +0800

    refactor(demand-loading): 优化代码

--- demand-loading/babel-plugin-demand-loading.js
+++ demand-loading/babel-plugin-demand-loading.js
@@ -1,24 +1,20 @@
 module.exports = function ({ types: t }) {
+  const genImportDeclaration = (specifier, libraryName) => {
+    return t.importDeclaration(
+      [t.importDefaultSpecifier(specifier.local)],
+      t.stringLiteral(`${libraryName}/${specifier.imported.name.replace(/^./, s => s.toLowerCase())}`)
+    )
+  }
+
   return {
     visitor: {
       ImportDeclaration(path, { opts: { libraryName } }) {
         if (!libraryName) return
-        if (path.node.source.value === libraryName) {
-          const { specifiers } = path.node
-          const findResult = specifiers.find(ele => ele.type === 'ImportDefaultSpecifier')
-          if (findResult) return
-          const genImportDeclaration = (specifiers) => {
-            return specifiers.map(specifier => {
-              return t.importDeclaration([
-                t.importDefaultSpecifier(
-                  specifier.local
-                )
-              ], t.stringLiteral(`${libraryName}/${specifier.imported.name.replace(/^./, s => s.toLowerCase())}`))
-            })
-          }
-          path.replaceWithMultiple(genImportDeclaration(specifiers))
-          return
-        }
+        if (path.node.source.value !== libraryName) return
+        const { specifiers } = path.node
+        const findResult = specifiers.find(ele => ele.type === 'ImportDefaultSpecifier')
+        if (findResult) return
+        path.replaceWithMultiple(specifiers.map(ele => genImportDeclaration(ele, libraryName)))
       }
     }
   }
