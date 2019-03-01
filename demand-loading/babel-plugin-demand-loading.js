20b76c2cd1daa41b6883c9f6dc439cf52f2488d9
chenyanfei-m <chenyanfei_m@foxmail.com>
2019-03-01 18:03:19 +0800

    refactor(demand-loading): 增加注释

--- demand-loading/babel-plugin-demand-loading.js
+++ demand-loading/babel-plugin-demand-loading.js
@@ -5,16 +5,23 @@ module.exports = function ({ types: t }) {
       t.stringLiteral(`${libraryName}/${specifier.imported.name.replace(/^./, s => s.toLowerCase())}`)
     )
   }
-
   return {
+    // 访问者模式, 通过对AST的遍历对访问到的ImportDeclaration作处理
     visitor: {
+      // 筛选特征1
       ImportDeclaration(path, { opts: { libraryName } }) {
+        // 如果没有指定moduleName
         if (!libraryName) return
+        // 如果语句的source value不为libraryName
         if (path.node.source.value !== libraryName) return
         const { specifiers } = path.node
-        const findResult = specifiers.find(ele => ele.type === 'ImportDefaultSpecifier')
-        if (findResult) return
-        path.replaceWithMultiple(specifiers.map(ele => genImportDeclaration(ele, libraryName)))
+        // 筛选特征
+        const allImportSpecifier = specifiers.every(ele => ele.type === 'ImportSpecifier')
+        if (!allImportSpecifier) return
+        // 遍历所有specifier, 将其属性传入genImportDeclaration函数生成一个或多个ImportDeclaration
+        const ImportDeclarations = specifiers.map(ele => genImportDeclaration(ele, libraryName))
+        // 替换原来的ImportDeclaration
+        path.replaceWithMultiple(ImportDeclarations)
       }
     }
   }
--- demand-loading/index.js
+++ demand-loading/index.js
@@ -1,3 +1,6 @@
+import ElementUI from 'element-ui'
+import { Card } from 'antd'
 import { Input as VInput, Select } from 'element-ui'
+
 Vue.use(VInput)
 Vue.use(Select)
